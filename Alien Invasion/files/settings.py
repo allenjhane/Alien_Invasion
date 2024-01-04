61af8178e5284f2b8d364a77570e7e5dc472b435
Jhane <120336667+allenjhane@users.noreply.github.com>
2024-01-04 20:18:25 +0100

    Add files via upload

---
+++ Alien Invasion/files/settings.py
@@ -0,0 +1,15 @@
+class Settings():
+    """class to store all game settings"""
+
+    def __init__(self):
+        """Initialize game settings"""
+
+        #Screen settings
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.bg_color = (0, 0, 0)
+
+        # Ship Settings
+        self.ship_speed = 1.5
+
+
